264928f0371d4b4fc5aa6fb992ae17c63821a0ee
Oleksandr Siukov <as-service2008@ukr.net>
2023-02-27 23:07:24 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+//console.log(galleryItems);
 
-console.log(galleryItems);
+const refGalleryImg = document.querySelector(".gallery");
+
+const galleryMarkup = createGalleryImg(galleryItems);
+
+refGalleryImg.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createGalleryImg(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join(" ");
+}
