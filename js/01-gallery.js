88ca0b373c8e500fb52cfdaa638d84ccab4efcfc
Oleksandr Siukov <as-service2008@ukr.net>
2023-02-28 16:32:35 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,12 +1,35 @@
 import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
 //console.log(galleryItems);
+
+//  Подключение стилей и скрипта модальгого окна
+const stylesModalWindow = document.createElement("link");
+stylesModalWindow.href =
+  "https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.css";
+stylesModalWindow.rel = "stylesheet";
+
+const documentHead = document.querySelector("head");
+documentHead.append(stylesModalWindow);
+
+const scriptModalWindow = document.createElement("script");
+scriptModalWindow.src =
+  "https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/dist/basicLightbox.min.js";
+
+const documentBody = document.querySelector("body");
+documentBody.append(scriptModalWindow);
+
+// ----------------------------------------
+
+let instance = [];
 
 const refGalleryImg = document.querySelector(".gallery");
 
 const galleryMarkup = createGalleryImg(galleryItems);
 
 refGalleryImg.insertAdjacentHTML("beforeend", galleryMarkup);
+
+refGalleryImg.addEventListener("click", onGalleryImgClick);
 
 function createGalleryImg(galleryItems) {
   return galleryItems
@@ -23,4 +46,28 @@ function createGalleryImg(galleryItems) {
 </div>`;
     })
     .join(" ");
+}
+
+function onGalleryImgClick(event) {
+  event.preventDefault();
+
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const refImage = event.target.dataset.source;
+
+  instance = basicLightbox.create(`
+    <img src="${refImage}" width="800" height="600">
+`);
+
+  instance.show();
+  document.addEventListener("keydown", offModalWindow);
+}
+
+function offModalWindow(event) {
+  if (event.key === "Escape") {
+    document.removeEventListener("keydown", offModalWindow);
+    instance.close();
+  }
 }
