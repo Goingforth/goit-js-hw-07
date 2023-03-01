cda899be8326aee0e1c94f068568fd00a3624a2e
Oleksandr Siukov <as-service2008@ukr.net>
2023-03-01 18:31:05 +0200

    task02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,49 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+//console.log(galleryItems);
+
+//  Подключение стилей и скрипта модальгого окна
+const stylesModalWindow = document.createElement("link");
+stylesModalWindow.href =
+  "https://cdnjs.cloudflare.com/ajax/libs/simplelightbox/2.12.1/simple-lightbox.min.css";
+stylesModalWindow.rel = "stylesheet";
+
+const documentHead = document.querySelector("head");
+documentHead.append(stylesModalWindow);
+
+const scriptModalWindow = document.createElement("script");
+scriptModalWindow.src =
+  "https://cdnjs.cloudflare.com/ajax/libs/simplelightbox/2.12.1/simple-lightbox.min.js";
+
+const documentBody = document.querySelector("body");
+documentBody.append(scriptModalWindow);
+
+// ----------------------------------------
+
+const refGalleryImg = document.querySelector(".gallery");
+
+const galleryMarkup = createGalleryImg(galleryItems);
+
+refGalleryImg.insertAdjacentHTML("beforeend", galleryMarkup);
+
+refGalleryImg.addEventListener("click", onGalleryImgClick);
+
+function createGalleryImg(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}"/>
+</a>`;
+    })
+    .join(" ");
+}
+
+function onGalleryImgClick(event) {
+  event.preventDefault();
+
+  let lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+}
